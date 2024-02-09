0ed3ff4ad96dff45b97f4e73cfab9595404cb5d0
LukaGakhokidze <lukagakhokidze@gmail.com>
2024-02-09 15:03:34 +0400

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import Email from "./Email";
-import Info from "./Info";
-import Success from "./Success";
+import { useState } from 'react';
+import Email from './Email';
+import Info from './Info';
+import Success from './Success';
 
 function App() {
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState('');
   const [isValid, setIsValid] = useState(true);
   const [submitted, setSubmitted] = useState(false);
 
@@ -25,26 +25,26 @@ function App() {
   };
 
   function handleDismiss() {
-    setEmail("");
+    setEmail('');
     setIsValid(true);
     setSubmitted(false);
   }
 
   return (
-    <div className="flex items-center justify-center w-screen h-screen overflow-hidden bg-charcoal_grey">
+    <div className='flex items-center justify-center w-screen h-screen overflow-hidden bg-charcoal_grey'>
       {submitted && isValid ? (
         <Success email={email} onDismiss={handleDismiss} />
       ) : (
-        <div className="app desktop: h-screen w-screen items-center justify-items-end rounded-[36px] bg-white desktop:grid desktop:h-4/6 desktop:w-8/12 desktop:grid-cols-2 desktop:grid-rows-1 desktop:pr-6">
+        <div className='app desktop: h-screen w-screen items-center justify-items-end rounded-[36px] bg-white desktop:grid desktop:h-4/6 desktop:w-8/12 desktop:grid-cols-2 desktop:grid-rows-1 desktop:pr-6'>
           <img
-            className="hidden mb-0 desktop:order-2 desktop:block"
-            src="../public/assets/images/illustration-sign-up-desktop.svg"
-            alt="illustration"
+            className='hidden mb-0 desktop:order-2 desktop:block'
+            src='../public/assets/images/illustration-sign-up-desktop.svg'
+            alt='illustration'
           />
           <img
-            className="mb-10 desktop:order-2 desktop:hidden"
-            src="../public/assets/images/illustration-sign-up-mobile.svg"
-            alt="illustration"
+            className='mb-10 desktop:order-2 desktop:hidden'
+            src='../public/assets/images/illustration-sign-up-mobile.svg'
+            alt='illustration'
           />
           <div>
             <Info />
