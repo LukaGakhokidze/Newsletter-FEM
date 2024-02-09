8648a882bd05628f815afb7af0b37d9bd1a70856
LukaGakhokidze <lukagakhokidze@gmail.com>
2024-02-09 15:38:50 +0400

    changes

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,31 +1,31 @@
 const listInfo = [
   {
     id: 1,
-    text: "Product discovery and building what matters",
+    text: 'Product discovery and building what matters'
   },
   {
     id: 2,
-    text: "Measuring to ensure updates are a success",
+    text: 'Measuring to ensure updates are a success'
   },
   {
     id: 3,
-    text: "And much more!",
-  },
+    text: 'And much more!'
+  }
 ];
 
 function Info() {
   return (
-    <div className="flex flex-col gap-6 px-6">
-      <h1 className="font-primary text-3xl font-bold text-dark_slate_grey desktop:text-[56px]">
+    <div className='flex flex-col gap-6 px-6'>
+      <h1 className='font-primary text-3xl font-bold text-dark_slate_grey desktop:text-[56px]'>
         Stay Updated !
       </h1>
 
       <p>Join 60,000+ product managers receiving monthly updates on:</p>
 
-      <ul className="desktop:mb-3">
+      <ul className='desktop:mb-3'>
         {listInfo.map((info) => (
-          <li key={info.id} className="flex items-center gap-4 ">
-            <img src="../public/assets/images/icon-list.svg" alt="" />
+          <li key={info.id} className='flex items-center gap-4 '>
+            <img src='assets/images/icon-list.svg' alt='' />
             <p>{info.text}</p>
           </li>
         ))}
