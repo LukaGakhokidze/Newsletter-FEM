42f49d25881b7150c62f99c643659819340941b6
LukaGakhokidze <lukagakhokidze@gmail.com>
2024-02-09 15:14:38 +0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Newsletter-FEM-1/',
+  base: '/Newsletter-FEM/',
   plugins: [react()]
 });
